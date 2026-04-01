2504ab31528bf92b6a15189cb57cb041b9f89c88
Edgardo <edgardochamorro08@gmail.com>
2026-03-31 22:06:10 -0600

    Add global /api prefix to all routes

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // Prefijo global para todas las rutas
+  app.setGlobalPrefix('api');
+  
   // Habilitar CORS para el frontend
   app.enableCors({
     origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
